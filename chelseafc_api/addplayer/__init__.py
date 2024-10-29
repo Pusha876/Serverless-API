724e68b99278c36df6931675b8fbfec957e129db
jamie pryce <djpusha876@gmail.com>
2024-10-29 11:00:05 -0400

    added uploader

---
+++ chelseafc_api/addplayer/__init__.py
@@ -0,0 +1,69 @@
+"""
+ This script will upload Chelsea FC players info to the database
+"""
+import os
+import logging
+import azure.functions as func
+from azure.cosmos import CosmosClient, PartitionKey
+
+
+def main() -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+
+# Initialize Cosmos DB client
+endpoint = os.getenv('COSMOS_DB_ENDPOINT')  # or replace with your endpoint
+key = os.getenv('COSMOS_DB_KEY')  # or replace with your key
+client = CosmosClient(endpoint, key)
+
+# Create database and container if they don't exist
+database_name = 'ChelseaFCDB'
+container_name = 'PlayersCollection'
+
+database = client.create_database_if_not_exists(id=database_name)
+container = database.create_container_if_not_exists(
+    id=container_name,
+    partition_key=PartitionKey(path="/id"),
+    offer_throughput=400
+)
+
+# Sample data for Chelsea FC players
+players = [
+    {
+        "id": "1",
+        "name": "Marc Cucurella Saseta",
+        "position": "Left-back, left wing-back",
+        "number": 3,
+        "Place of birth": "Alella, Spain",
+        "dob": "07-22-1998",
+        "coverUrl": "url-to-image-in-cloud-storage"
+    },
+
+    {
+        "id": "2",
+        "name": "Moises Isaac Caicedo Corozo",
+        "position": "Defensive midfielder",
+        "number": 25,
+        "Place of birth": "Santo Domingo, Ecuador",
+        "dob": "11-02-2001",
+        "coverUrl": "url-to-image-in-cloud-storage"
+    },
+
+    {
+        "id": "3",
+        "name": "Cole Jermaine Palmer",
+        "position": "Attacking midfielder, winger",
+        "number": 20,
+        "Place of birth": "Manchester, England",
+        "dob": "05-06-2002",
+        "coverUrl": "url-to-image-in-cloud-storage"
+    }
+    # Add more players here
+
+]
+
+# Insert players into Cosmos DB
+for player in players:
+    container.create_item(body=player)
+
+print("Player data successfully uploaded to Cosmos DB!")
